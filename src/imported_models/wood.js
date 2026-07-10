b20b5b5017edca73833affd2b9a3bbe5204c83f2
martinaligorio <116801988+martinaligorio@users.noreply.github.com>
2026-07-11 00:21:12 +0200

    fixed path

--- src/imported_models/wood.js
+++ src/imported_models/wood.js
@@ -183,6 +183,7 @@ export function loadWoodTask(scene) {
     new Promise((resolve) => {
       const mtlLoader = new MTLLoader();
       mtlLoader.setPath('./models_optimized/');
+      mtlLoader.setResourcePath('/');
 
       mtlLoader.load(
         '12303_Firewood_Stack_v1_l3.mtl',
